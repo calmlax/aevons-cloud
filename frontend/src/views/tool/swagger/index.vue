2216843641b75be39a43313d6bd4b7ee22680541
Jing <1073602@qq.com>
2026-05-19 09:23:23 +0800

    desc

---
+++ frontend/src/views/tool/swagger/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="page-stack swagger-page">
+    <a-card class="panel-card swagger-card" :bordered="false">
+      <iframe
+        class="swagger-frame"
+        title="Swagger UI"
+        src="http://localhost:11080/swagger/"
+      />
+    </a-card>
+  </div>
+</template>
+
+<style scoped>
+.swagger-page {
+  min-height: calc(100vh - 120px);
+  display: flex;
+}
+
+.swagger-card {
+  flex: 1;
+  display: flex;
+  min-height: 0;
+}
+
+:deep(.swagger-card .arco-card-body) {
+  flex: 1;
+  display: flex;
+  padding: 0;
+  min-height: 0;
+}
+
+.swagger-frame {
+  flex: 1;
+  width: 100%;
+  min-height: calc(100vh - 160px);
+  border: 0;
+  display: block;
+}
+</style>
